dec77ca7e63045a12436329d62c52b3f642d8e25
Zachary Kaczor <zachary@kaczorzoo.net>
2018-10-17 17:19:27 -0400

    Added levels and smarter rendering

--- game.py
+++ game.py
@@ -12,15 +12,25 @@ class GameWindow(pyglet.window.Window):
 
         super().__init__(*args, **kwargs)
 
-        # Set size of maze and cells.
+        # Set size of the maze, the cells, and multiplier.
         self.cell = 20
-        self.cols = 16
-        self.rows = 16
+        self.cols = 5
+        self.rows = 5
+        self.mult = 1.25
 
         # Generate maze and start at the bottom left.
         self.maze = Maze(self.cols, self.rows)
         self.maze.generate()
         self.cell_index = 0
+
+        # Score label
+        self.score_label = pyglet.text.Label(
+            "0",
+            font_name='Times New Roman',
+            font_size=36,
+            x=self.width//2, y=self.height - 50,
+            anchor_x='center', anchor_y='center'
+        )
 
     def on_key_press(self, symbol, modifiers):
         """
@@ -46,6 +56,18 @@ class GameWindow(pyglet.window.Window):
         if (symbol in [py_keyboard.A, py_keyboard.LEFT]) and (not player_cell.walls[3]):
             self.cell_index -= self.cols
 
+        # Handle level complete
+        if (self.cols * self.rows) - 1 == self.cell_index:
+            self.cols = int(self.cols * self.mult)
+            self.rows = int(self.rows * self.mult)
+            self.maze = Maze(self.cols, self.rows)
+
+            self.maze.generate()
+            self.cell_index = 0
+
+            self.score_label.text = str(int(self.score_label.text) + 1)
+            print("Level Complete!")
+
     def on_draw(self):
         """
         Render the maze.
@@ -53,6 +75,9 @@ class GameWindow(pyglet.window.Window):
 
         # Clear the screen.
         self.clear()
+
+        # Draw score
+        self.score_label.draw()
 
         # Get the center of the screen in cell coordinates.
         center_x = (self.width  // 2) / self.cell
@@ -63,65 +88,69 @@ class GameWindow(pyglet.window.Window):
 
         # Iterate over all cells in the maze
         for cell in self.maze.grid:
-            # Get the correct render coordinates for the cell.
-            render_x = (cell.x + center_x - player_cell.x) * self.cell
-            render_y = (cell.y + center_y - player_cell.y) * self.cell
+            if abs(cell.x - player_cell.x) < self.height/self.cell/2 and abs(cell.y - player_cell.y) < self.width/self.cell/2:
+                # Get the correct render coordinates for the cell.
+                render_x = (cell.x + center_x - player_cell.x) * self.cell
+                render_y = (cell.y + center_y - player_cell.y) * self.cell
 
-            # Handle rendering for the top wall.
-            if cell.walls[0]:
-                pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                    ("v2f", (
-                        render_x, 
-                        render_y + self.cell, 
-                        render_x + self.cell, 
-                        render_y + self.cell
-                    ))
-                )
+                # Handle rendering for the top wall.
+                if cell.walls[0]:
+                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                        ("v2f", (
+                            render_x, 
+                            render_y + self.cell, 
+                            render_x + self.cell, 
+                            render_y + self.cell
+                        ))
+                    )
 
-            # Handle rendering for the right wall.
-            if cell.walls[1]:
-                pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                    ("v2f", (
-                        render_x + self.cell, 
-                        render_y + self.cell, 
-                        render_x + self.cell, 
-                        render_y
-                    ))
-                )
+                # Handle rendering for the right wall.
+                if cell.walls[1]:
+                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                        ("v2f", (
+                            render_x + self.cell, 
+                            render_y + self.cell, 
+                            render_x + self.cell, 
+                            render_y
+                        ))
+                    )
 
-            # Handle rendering for the bottom wall.
-            if cell.walls[2]:
-                pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                    ("v2f", (
-                        render_x, 
-                        render_y, 
-                        render_x + self.cell, 
-                        render_y
-                    ))
-                )
+                # Handle rendering for the bottom wall.
+                if cell.walls[2]:
+                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                        ("v2f", (
+                            render_x, 
+                            render_y, 
+                            render_x + self.cell, 
+                            render_y
+                        ))
+                    )
 
-            # Handle rendering for the left wall.
-            if cell.walls[3]:
-                pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                    ("v2f", (
-                        render_x, 
-                        render_y, 
-                        render_x, 
-                        render_y + self.cell
-                    ))
-                )
+                # Handle rendering for the left wall.
+                if cell.walls[3]:
+                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                        ("v2f", (
+                            render_x, 
+                            render_y, 
+                            render_x, 
+                            render_y + self.cell
+                        ))
+                    )
 
         # Convert the center coordinates to pixel coordinates.
         render_x = center_x * self.cell
         render_y = center_y * self.cell
+        
+        # The pixel offset from the edge of the cell.
+        offset = 4
 
         # Draw the player square in the center of the screen.
         pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
             ("v2f", (
-                render_x, render_y,
-                render_x, render_y + self.cell,
-                render_x + self.cell, render_y + self.cell,
-                render_x + self.cell, render_y
+                render_x + offset, render_y + offset,
+                render_x + offset, render_y + self.cell - offset,
+                render_x + self.cell - offset, render_y + self.cell - offset,
+                render_x + self.cell - offset, render_y + offset
             ))
         )
 
