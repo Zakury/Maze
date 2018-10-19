1486f8a976a7c22175b0082ea7625880763e8005
Zachary Kaczor <zachary@kaczorzoo.net>
2018-10-19 15:56:36 -0400

    Changed tile system to a 2d array

--- game.py
+++ game.py
@@ -12,58 +12,71 @@ class GameWindow(pyglet.window.Window):
 
         super().__init__(*args, **kwargs)
 
-        # Set size of the maze, the cells, and multiplier.
-        self.cell = 20
-        self.cols = 5
-        self.rows = 5
-        self.mult = 1.25
+        # Set the size of the maze, cells, and multiplier.
+        self.maze_size = 5
+        self.cell_size = 20
+        self.size_mult = 1.20
 
         # Generate maze and start at the bottom left.
-        self.maze = Maze(self.cols, self.rows)
-        self.maze.generate()
-        self.cell_index = 0
+        self.maze = Maze(self.maze_size)
+        self.maze.generate_maze()
+        self.cell_cur = self.maze.cell(0, 0)
 
         # Score label
         self.score_label = pyglet.text.Label(
             "0",
             font_name='Times New Roman',
-            font_size=36,
+            font_size=40,
             x=self.width//2, y=self.height - 50,
             anchor_x='center', anchor_y='center'
         )
+
+    def move_cell(self, direction):
+        """
+        Move to the next cell in a direction.
+        """
+
+        pass
 
     def on_key_press(self, symbol, modifiers):
         """
         Handles user input for movement.
         """
 
+        movement_bindings = {
+            "n": (py_keyboard.W, py_keyboard.UP),
+            "e": (py_keyboard.D, py_keyboard.RIGHT),
+            "s": (py_keyboard.S, py_keyboard.DOWN),
+            "w": (py_keyboard.A, py_keyboard.LEFT)
+        }
+
         # Get the player's cell.
-        player_cell = self.maze.grid[self.cell_index]
+        player_cell = self.cell_cur
 
         # Handle movement going up.
         if (symbol in [py_keyboard.W, py_keyboard.UP]) and (not player_cell.walls[0]):
-            self.cell_index += 1
+            self.cell_cur = self.maze.cell(self.cell_cur.x, self.cell_cur.y + 1)
 
         # Handle movement going right.
         if (symbol in [py_keyboard.D, py_keyboard.RIGHT]) and (not player_cell.walls[1]):
-            self.cell_index += self.cols
+            self.cell_cur = self.maze.grid[self.cell_cur.y][self.cell_cur.x + 1]
         
         # Handle movement going down.
         if (symbol in [py_keyboard.S, py_keyboard.DOWN]) and (not player_cell.walls[2]):
-            self.cell_index -= 1
+            self.cell_cur = self.maze.grid[self.cell_cur.y - 1][self.cell_cur.x]
 
         # Handle movement going left.
         if (symbol in [py_keyboard.A, py_keyboard.LEFT]) and (not player_cell.walls[3]):
-            self.cell_index -= self.cols
+            self.cell_cur = self.maze.grid[self.cell_cur.y][self.cell_cur.x - 1]    
 
         # Handle level complete
-        if (self.cols * self.rows) - 1 == self.cell_index:
-            self.cols = int(self.cols * self.mult)
-            self.rows = int(self.rows * self.mult)
-            self.maze = Maze(self.cols, self.rows)
-
-            self.maze.generate()
-            self.cell_index = 0
+        if self.cell_cur.x == self.maze_size - 1 and self.cell_cur.y == self.maze_size - 1:
+            self.maze_size = int(self.maze_size * self.size_mult)
+            # self.maze = Maze(self.maze_size)
+            self.maze.size = self.maze_size
+            self.maze.generate_grid()
+            self.maze.generate_maze()
+            self.cell_cur = self.maze.cell(0, 0)
 
             self.score_label.text = str(int(self.score_label.text) + 1)
             print("Level Complete!")
@@ -80,66 +93,67 @@ class GameWindow(pyglet.window.Window):
         self.score_label.draw()
 
         # Get the center of the screen in cell coordinates.
-        center_x = (self.width  // 2) / self.cell
-        center_y = (self.height // 2) / self.cell
+        center_x = (self.width  // 2) / self.cell_size
+        center_y = (self.height // 2) / self.cell_size
 
         # Get the player's cell.
-        player_cell = self.maze.grid[self.cell_index]
+        player_cell = self.cell_cur
 
         # Iterate over all cells in the maze
-        for cell in self.maze.grid:
-            if abs(cell.x - player_cell.x) < self.height/self.cell/2 and abs(cell.y - player_cell.y) < self.width/self.cell/2:
-                # Get the correct render coordinates for the cell.
-                render_x = (cell.x + center_x - player_cell.x) * self.cell
-                render_y = (cell.y + center_y - player_cell.y) * self.cell
+        for row in self.maze.grid:
+            for cell in row:
+                if abs(cell.x - player_cell.x) < self.height/self.cell_size/2 and abs(cell.y - player_cell.y) < self.width/self.cell_size/2:
+                    # Get the correct render coordinates for the cell.
+                    render_x = (cell.x + center_x - player_cell.x) * self.cell_size
+                    render_y = (cell.y + center_y - player_cell.y) * self.cell_size
 
-                # Handle rendering for the top wall.
-                if cell.walls[0]:
-                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                        ("v2f", (
-                            render_x, 
-                            render_y + self.cell, 
-                            render_x + self.cell, 
-                            render_y + self.cell
-                        ))
-                    )
+                    # Handle rendering for the top wall.
+                    if cell.walls[0]:
+                        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                            ("v2f", (
+                                render_x, 
+                                render_y + self.cell_size, 
+                                render_x + self.cell_size, 
+                                render_y + self.cell_size
+                            ))
+                        )
 
-                # Handle rendering for the right wall.
-                if cell.walls[1]:
-                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                        ("v2f", (
-                            render_x + self.cell, 
-                            render_y + self.cell, 
-                            render_x + self.cell, 
-                            render_y
-                        ))
-                    )
+                    # Handle rendering for the right wall.
+                    if cell.walls[1]:
+                        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                            ("v2f", (
+                                render_x + self.cell_size, 
+                                render_y + self.cell_size, 
+                                render_x + self.cell_size, 
+                                render_y
+                            ))
+                        )
 
-                # Handle rendering for the bottom wall.
-                if cell.walls[2]:
-                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                        ("v2f", (
-                            render_x, 
-                            render_y, 
-                            render_x + self.cell, 
-                            render_y
-                        ))
-                    )
+                    # Handle rendering for the bottom wall.
+                    if cell.walls[2]:
+                        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                            ("v2f", (
+                                render_x, 
+                                render_y, 
+                                render_x + self.cell_size, 
+                                render_y
+                            ))
+                        )
 
-                # Handle rendering for the left wall.
-                if cell.walls[3]:
-                    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                        ("v2f", (
-                            render_x, 
-                            render_y, 
-                            render_x, 
-                            render_y + self.cell
-                        ))
-                    )
+                    # Handle rendering for the left wall.
+                    if cell.walls[3]:
+                        pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+                            ("v2f", (
+                                render_x, 
+                                render_y, 
+                                render_x, 
+                                render_y + self.cell_size
+                            ))
+                        )
 
         # Convert the center coordinates to pixel coordinates.
-        render_x = center_x * self.cell
-        render_y = center_y * self.cell
+        render_x = center_x * self.cell_size
+        render_y = center_y * self.cell_size
         
         # The pixel offset from the edge of the cell.
         offset = 4
@@ -148,9 +162,9 @@ class GameWindow(pyglet.window.Window):
         pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
             ("v2f", (
                 render_x + offset, render_y + offset,
-                render_x + offset, render_y + self.cell - offset,
-                render_x + self.cell - offset, render_y + self.cell - offset,
-                render_x + self.cell - offset, render_y + offset
+                render_x + offset, render_y + self.cell_size - offset,
+                render_x + self.cell_size - offset, render_y + self.cell_size - offset,
+                render_x + self.cell_size - offset, render_y + offset
             ))
         )
 
--- maze.py
+++ maze.py
@@ -23,14 +23,14 @@ class Cell:
         # If the cell is the current one.
         self.current = False
 
-    def remove_walls(self, other_index):
+    def remove_walls(self, other_cell):
         """
         Remove the wall of the cell in the direction of the other cell.
         """
 
         # Convert the cell index into x and y coordinates.
-        other_x = self.maze.grid[other_index].x
-        other_y = self.maze.grid[other_index].y
+        other_x = other_cell.x
+        other_y = other_cell.y
 
         # If the other cell is up, remove the top wall.
         if self.y < other_y:
@@ -50,22 +50,24 @@ class Cell:
         
 class Maze:
 
-    def __init__(self, cols, rows):
-        # Create the grid and save the maze size.
-        self.grid = [Cell(self, x, y) for x in range(cols) for y in range(rows)]
-        self.cols = cols
-        self.rows = rows
+    def __init__(self, size):
+
+        # Save map size.
+        self.size = size
+
+        # Create the grid.
+        self.generate_grid()
 
         # Select the bottom left cell and create the stack.
-        self.select_cell(0)
+        self.select_cell(self.cell(0, 0))
         self.stack = []
 
-    def index(self, x, y):
+    def cell(self, x, y):
         """
         Get the index of a certain position in the grid array.
         """
 
-        return y + x * self.cols
+        return self.grid[y][x]
 
     def get_adjacent(self):
         """
@@ -80,17 +82,17 @@ class Maze:
             Check if a cell is valid.
             """
 
-            cell_index = self.index(x, y)
-
             # Check if the cell is inbounds. 
-            if x >= 0 and x < self.cols and y >= 0 and y < self.rows:
+            if x >= 0 and x < self.size and y >= 0 and y < self.size:
+                cell = self.cell(x, y)
                 # Check if the cell hasn't been visited
-                if not self.grid[cell_index].visited:
+                if not cell.visited:
                     # Push the cell to the valid cell list.
-                    valid_cells.append(cell_index)
+                    valid_cells.append(cell)
 
         # Get the x and y values of the current cell.
-        x, y = self.grid[self.current].x, self.grid[self.current].y
+        x = self.current.x
+        y = self.current.y
 
         # Validate the cells in the four directions.
         validate(x, y + 1)
@@ -101,25 +103,41 @@ class Maze:
         # Return the valid cells.
         return valid_cells
 
-    def select_cell(self, index):
+    def select_cell(self, cell):
         """
         Select a cell index as the current one.
         """
 
-        # Mark the cell as visited.
-        self.grid[index].visited = True
+        cell.visited = True # Mark the cell as visited
+        self.current = cell # Mark the cell as current
 
-        # Set the cell as the current one.
-        self.current = index
+    def generate_grid(self):
+        """
+        Generate the grid.
+        """
 
-    def generate(self):
+        self.grid = []
+        for y in range(self.size):
+            self.grid.append([])
+            for x in range(self.size):
+                cell = Cell(self, x, y)
+                self.grid[-1].append(cell)
+
+    def generate_maze(self):
         """
         Generate the maze using:
         https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
         """
 
+        def unvisited_cell(maze):
+            items = []
+            for row in self.grid:
+                for cell in row:
+                    items.append(cell.visited)
+            return any(not visited for visited in items)
+
         # While any unvisited cells in the grid.
-        while any(not cell.visited for cell in self.grid):
+        while unvisited_cell(self):
             # Get neighbors that has not been visited or are out of bounds.
             adjacents = self.get_adjacent()
 
@@ -132,13 +150,15 @@ class Maze:
                 self.stack.append(self.current)
                 
                 # Remove the wall between the current cell and the next cell.
-                self.grid[self.current].remove_walls(next_cell)
-                self.grid[next_cell].remove_walls(self.current)
+                self.current.remove_walls(next_cell)
+                next_cell.remove_walls(self.current)
                 
                 # Select the next cell.
-                self.select_cell(next_cell)      
+                self.select_cell(next_cell)
 
             # Otherwise if the stack is not empty,         
             elif self.stack:
                 # Pop a cell from the stack and select it.
                 self.select_cell(self.stack.pop())
+        else:
+            return
